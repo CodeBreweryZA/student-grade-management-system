69ccc935884041dff998d2804046430223f2c946
landiso phaahla <landisodimpho.phaahla@gmail.com>
2026-07-03 18:32:31 +0200

    Add StudentManager class with core functions

--- StudentManager.h
+++ StudentManager.h
@@ -1,1 +1,33 @@
+#ifndef STUDENTMANAGER_H
+#define STUDENTMANAGER_H
 
+#include <vector>
+#include "Student.h"
+
+using namespace std;
+
+class StudentManager
+{
+private:
+    vector<Student> students;
+
+public:
+    // Constructor
+    StudentManager();
+
+    // Core Functions
+    void addStudent();
+    void viewStudents();
+    void searchStudent();
+    void updateStudent();
+    void deleteStudent();
+
+    // File Handling
+    void saveToFile();
+    void loadFromFile();
+
+    // Menu
+    void showMenu();
+};
+
+#endif
