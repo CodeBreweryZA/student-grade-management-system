5a908ae4ca8c139d8e73d617805468e35fe3b5a0
landiso phaahla <landisodimpho.phaahla@gmail.com>
2026-07-03 18:33:09 +0200

    Implement basic structure for StudentManager class

--- StudentManager.cpp
+++ StudentManager.cpp
@@ -1,1 +1,62 @@
+#include "../include/StudentManager.h"
+#include <iostream>
 
+using namespace std;
+
+// Constructor
+StudentManager::StudentManager()
+{
+}
+
+// Display Menu
+void StudentManager::showMenu()
+{
+    cout << "\n======================================" << endl;
+    cout << "   STUDENT MANAGEMENT SYSTEM" << endl;
+    cout << "======================================" << endl;
+    cout << "1. Add Student" << endl;
+    cout << "2. View Students" << endl;
+    cout << "3. Search Student" << endl;
+    cout << "4. Update Student" << endl;
+    cout << "5. Delete Student" << endl;
+    cout << "6. Save to File" << endl;
+    cout << "7. Load from File" << endl;
+    cout << "0. Exit" << endl;
+    cout << "======================================" << endl;
+}
+
+// Placeholder Functions
+void StudentManager::addStudent()
+{
+    cout << "Add Student - Coming Soon..." << endl;
+}
+
+void StudentManager::viewStudents()
+{
+    cout << "View Students - Coming Soon..." << endl;
+}
+
+void StudentManager::searchStudent()
+{
+    cout << "Search Student - Coming Soon..." << endl;
+}
+
+void StudentManager::updateStudent()
+{
+    cout << "Update Student - Coming Soon..." << endl;
+}
+
+void StudentManager::deleteStudent()
+{
+    cout << "Delete Student - Coming Soon..." << endl;
+}
+
+void StudentManager::saveToFile()
+{
+    cout << "Save to File - Coming Soon..." << endl;
+}
+
+void StudentManager::loadFromFile()
+{
+    cout << "Load from File - Coming Soon..." << endl;
+}
